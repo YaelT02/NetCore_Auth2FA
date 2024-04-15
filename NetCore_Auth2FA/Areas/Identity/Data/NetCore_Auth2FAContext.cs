3886048e2a767fdde06042c1f6fc58d9f7c0b313
SERGIO YAEL TREJO MAYA <sergioytrejo02@hotmail.com>
2024-04-14 22:32:52 -0600

    Agregar archivos de proyecto.

---
+++ NetCore_Auth2FA/Areas/Identity/Data/NetCore_Auth2FAContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NetCore_Auth2FA.Data;
+
+public class NetCore_Auth2FAContext : IdentityDbContext<IdentityUser>
+{
+    public NetCore_Auth2FAContext(DbContextOptions<NetCore_Auth2FAContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
